6d1c0804f0b8c68c9a56f39b830bf7394c24acf1
Your Name <your_email@whatever.com>
2026-09-05 11:42:04 +0300

    Updated code to web server

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-console.log("Романюк Илья Сергеевич"); 
-console.log("401"); 
-let pi = 0;
-for (let i = 0; i < 2000000; i++) {
-    pi += (Math.pow(-1, i)) / (2 * i + 1);
-}
-pi = pi * 4;
+const http = require('http'); 
 
-console.log("Число ПИ: " + pi);
+let pi = 0;
+for (let i = 1; i <= 500; i++) {
+    let n = i * 2;
+    let term = 4 / (n * (n + 1) * (n + 2));
+    if (i % 2 === 1) {
+        pi += term;
+    } else {
+        pi -= term;
+    }
+}
+pi = 3 + pi;
+
+
+const journalNumber = 5; 
+const factor = Math.pow(10, journalNumber);
+const roundedPi = Math.round(pi * factor) / factor;
+
+const server = http.createServer((req, res) => {
+    
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    
+   
+    res.write("Иванов Иван Иванович\n");
+    res.write("Группа ИСП-311\n");
+    res.write("Число ПИ: " + roundedPi + "\n");
+    
+    res.end(); 
+});
+
+server.listen(3000, () => {
+    console.log("Сервер успешно запущен в вебе на http://localhost:3000");
+});
